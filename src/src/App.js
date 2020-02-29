7ad43e4010996da2d8a07b347d7be41091e33d3c
langyo <langyo.china@gmail.com>
2020-02-29 23:49:58 +0800

    .

--- src/src/App.js
+++ src/src/App.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
+
 import {
   Grid,
   Button,
-  TextField
+  TextField,
+  Paper
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+
+import {
+  mdiCardsSpade,
+  mdiCardsClub,
+  mdiCardsDiamond,
+  mdiCardsHeart
+} from '@mdi/js';
+import Icon from '@mdi/react';
 
 export default () => {
   const classes = makeStyles(theme => ({
@@ -19,16 +29,24 @@ export default () => {
   return <div className={classes.root}>
     <Grid container spacing={4}>
       <Grid item xs={3}>
-        <TextField variant='outlined' value='test' />
+        <Paper>
+          <Icon path={mdiCardsSpade} color='#333' />
+        </Paper>
       </Grid>
       <Grid item xs={3}>
-        <TextField variant='outlined' value='test' />
+        <Paper>
+          <Icon path={mdiCardsClub} color='#333' />
+        </Paper>
       </Grid>
       <Grid item xs={3}>
-        <TextField variant='outlined' value='test' />
+        <Paper>
+          <Icon path={mdiCardsDiamond} color='#936' />
+        </Paper>
       </Grid>
       <Grid item xs={3}>
-        <TextField variant='outlined' value='test' />
+        <Paper>
+          <Icon path={mdiCardsHeart} color='#936' />
+        </Paper>
       </Grid>
     </Grid>
   </div>;
